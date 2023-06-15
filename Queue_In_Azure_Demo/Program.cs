a43ba678614323fd75a0ad4995e85ee28d0db171
fellly <fellyka@gmail.com>
2023-06-15 22:02:51 +0200

    Add Message to an existing Azure Queue

--- Queue_In_Azure_Demo/Program.cs
+++ Queue_In_Azure_Demo/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Azure.Storage.Queues;
+
+using System;
 
 namespace Queue_In_Azure_Demo
 {
-    internal class Program
+    public class Program
     {
+        private static string StorageAccountConnectionString = "DefaultEndpointsProtocol=https;AccountName=stsollersaccount;AccountKey=ajQ5sGYUVY5cVjpANpHeWxWkvF8cU7cO5OLn3Fao4kxdhJ9PNLgEYW+2ghSwIJ6O/QeXr9YADFrq+ASt3FYNYw==;EndpointSuffix=core.windows.net";
+        private static string qName = "appqueuedemo";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueueClient qClient = new QueueClient(StorageAccountConnectionString, qName);
+
+            string messageToQueue = String.Empty;
+            if(qClient.Exists())
+            {
+                for (int i = 0; i <= 6; i++)
+                {
+                    messageToQueue = $"This is Message number {i+1}";
+                    qClient.SendMessage(messageToQueue);
+                }
+            }
+            Console.WriteLine("Message succesfully sent!");
+            Console.ReadLine();
         }
     }
 }
